48e4ad3b39bbb79894a21d11ecec00fe24809207
leandroledesma <leandro.d.ledesma@gmail.com>
2018-10-18 17:50:48 -0300

    first commit

--- server.js
+++ server.js
@@ -3,34 +3,40 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var app = express()
 
-var apiKey = '80c1245dc664673c681da5a0f58cf629';
+var apiKey = '762e3496ca09b07fc2e5a5f4c8a0ac1b';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
+var provinces = {};
 
 app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null});
+  request('https://apis.datos.gob.ar/georef/api/provincias', function(err,response,data){
+  provinces = JSON.parse(data) 
+  res.render('index', {weather: null, error: null, provinces: provinces});
+  })
 })
 
 app.post('/', function (req, res) {
-  var city = req.body.city;
+  var city = JSON.parse(req.body.city);
+  var lat = city.lat;
+  var lon = city.lon;
   // var url = `https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=${apiKey}`
-  var url = 'https://api.openweathermap.org/data/2.5/find?q='+city+'&units=metric&appid='+apiKey;
+  var url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   request(url, function (err, response, data) {
     if(err){
-      res.render('index', {weather: null, error: err});
+      res.render('index', {weather: null, error: err, provinces: provinces});
     } else {
       //console.log(data)
-      var weather = JSON.parse(data)
+      var weather = JSON.parse(data);
 
       if(weather.cod != 200){
-        res.render('index', {weather: null, error: 'Error, please try again'});
+        res.render('index', {weather: null, error: 'Error, please try again', provinces: provinces});
       } else {
-        var w = weather.list[0];
-        console.log(w)
-        var weatherText = `It's ${w.main.temp} degrees in ${w.name}!`;
-        res.render('index', {weather: weatherText, error: null});
+        //var w = weather.list[0];
+        //console.log(w)
+        var weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null, provinces: provinces});
       }
     }
   });
